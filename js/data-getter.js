07ba1d4173e44c07da854bd417427fdae67b364f
beyond-danube <alexey.snv@gmail.com>
2022-11-10 00:00:42 +0200

    Init - POC on obtaining data on particular period from krm-data repo

---
+++ js/data-getter.js
@@ -0,0 +1,14 @@
+const BASE_URL = 'https://raw.githubusercontent.com/beyond-danube/kmr-data/main/'
+
+async function fetchData(path) {
+    let response = await fetch(path)
+    return await response.json()
+}
+
+async function getDateMap() {
+    return await fetchData(BASE_URL + 'date-files-map/date-map.json')
+}
+
+async function getDataFile(fileName) {
+    return await fetchData(`https://raw.githubusercontent.com/beyond-danube/kmr-data/main/data-final/${fileName}`)
+}
