7ca0cf02753026f127c7ab351aa68cc9531c0486
beyond-danube <alexey.snv@gmail.com>
2022-11-15 00:15:52 +0200

    Add description on what is the data about

---
+++ js/doc-classes.js
@@ -0,0 +1,23 @@
+class VotingResultEntry {
+    DocTime
+    OrgName
+    SName
+    GLType
+    GLTime
+    PD_NPP
+    PD_Fullname
+    GL_Text
+    GL_ResultType
+    DPList
+    YESCnt
+    NOCnt
+    UTRCnt
+    NGCnt
+    TotalCnt
+    RESULT
+}
+
+class VotingEntry {
+    DPName
+    DPGolos
+}
